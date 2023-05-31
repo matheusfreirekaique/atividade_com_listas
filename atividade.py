1e605598da2fa0fab148401a22f28b293179d984
Matheus Freire <santosmatheus99@hotmail.com>
2023-05-30 21:30:00 -0300

    Atividades01

---
+++ atividade.py
@@ -0,0 +1,118 @@
+# aula de python XPE
+#a = 2
+#b = 3
+#c = 10
+#print(a / b ** c)
+
+#salario = 1500
+#aumento = 5
+#print(salario+(salario * aumento / 100))
+
+
+#numero = 5
+
+#menores = []
+#maiores = []
+
+#for i in range(10):
+    #if (i < numero):
+        #menores.append(i)
+    #elif (i > numero):
+        #maiores.append(i)
+        
+
+#print('Resultado final')
+#print('menores:', menores)
+#print('maiores:',maiores)
+
+#x = 4
+#if x > 5:
+    #x = x* 2
+#print(x)
+#print('fim')
+
+#x = 10
+#print(type(x))
+
+#x = 'matheus'
+#print(type(x))
+
+#print(type(3.14))
+
+#print(type(True))
+
+#x = None
+#print(type(x))
+
+#print(type(10+6))
+
+#print(type(5+3.9))
+
+#print(10+False)
+
+#print(True+5)
+
+#print(True+True+False)
+
+#B = 15
+#b = 10
+#h = 8
+#area =(B+b)*h/2
+#print('Area do Trapezio:',area)
+
+
+#g= 10 >= 5
+#print(g)
+
+#f = 25 < 6
+#print(f)
+
+# verificara se a pessao possui idade para dirigir
+
+idade = 25
+possui_cnh = False
+print ( idade > 18 and possui_cnh)
+
+
+#verificar se um passageiro pode viajar sozinho
+
+idade = 15
+possui_autorização = True
+print(idade > 18 or possui_autorização)
+
+# com base na tabela da verdade 
+
+print(True or False)
+print((11>1) and True)
+print(not(True or False))
+print(not('matheus'=='matheus'))
+
+# Uso da String
+
+palavra ='consolação'
+
+#indice positivo
+
+print('primeiro caracter', palavra[0])
+print('segundo caracter', palavra[1])
+print('sexto caracter', palavra [5])
+print('ultimo caracter',palavra [9])
+
+
+
+print('Ultimo caracter', palavra[-1])
+print('Penualtimo caracter', palavra[-2])
+print('Antipenultimo caracter',palavra [-3])
+print(' Primeio caracter',palavra [-10])
+
+
+
+
+
+mat=[
+    [80, 185, 31],
+    [70, 170, 25]
+]
+
+linha = mat[1]
+
